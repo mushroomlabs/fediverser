545318d5e2c72890266eec8e855d7d28c6d7ff19
Raphael Lullis <raphael@mushroomlabs.com>
2024-07-22 12:24:02 +0200

    Lint

--- fediverser/apps/core/templatetags/fediverser.py
+++ fediverser/apps/core/templatetags/fediverser.py
@@ -6,7 +6,6 @@ from django.utils.translation import gettext_lazy as _
 from wagtail.admin.ui import sidebar
 from wagtail.telepath import JSContext, adapter
 
-from fediverser.apps.core.models.activitypub import Community
 from fediverser.apps.core.models.feeds import Entry
 from fediverser.apps.core.models.mapping import ChangeRequest
 from fediverser.apps.core.models.network import FediversedInstance
